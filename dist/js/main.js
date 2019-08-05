1128fed16fddb673669a5c9be46f83a27d49d117
Ippo <rbgoco@gmail.com>
2019-08-05 14:55:01 +1000

    Netlify

--- dist/js/main.js
+++ dist/js/main.js
@@ -25,7 +25,7 @@ function toggleMenu() {
     menu.classList.remove("show");
     menuNav.classList.remove("show");
     menuBranding.classList.remove("show");
-    navItems.forEach(item => item.classList.aremoved("show"));
+    navItems.forEach(item => item.classList.removed("show"));
 
     // Set Menu State
     showMenu = false;
